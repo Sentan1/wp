844c2a47fb1ad98c809d06a0e920037a970ca6c1
Huh <sentrooper@gmail.com>
2025-10-29 19:59:42 +1100

    image fixing again

--- a3/index.php
+++ a3/index.php
@@ -30,9 +30,21 @@ $defaultImg = 'assets/images/skills/1.png';
         <?php endforeach; ?>
       <?php else: ?>
         <div class="carousel-item active">
-          <img src="assets/images/skills_banner.png" class="d-block w-100" alt="No skills yet">
+          <img src="assets/images/skills/8.png" class="d-block w-100" alt="Intro to PHP & MySQL">
           <div class="carousel-caption d-none d-md-block bg-dark bg-opacity-50 rounded">
-            <h5 class="text-white">No skills yet</h5>
+            <h5 class="text-white">Intro to PHP & MySQL</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="assets/images/skills/1.png" class="d-block w-100" alt="Beginner Guitar Lessons">
+          <div class="carousel-caption d-none d-md-block bg-dark bg-opacity-50 rounded">
+            <h5 class="text-white">Beginner Guitar Lessons</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="assets/images/skills/6.png" class="d-block w-100" alt="Digital Illustration with Procreate">
+          <div class="carousel-caption d-none d-md-block bg-dark bg-opacity-50 rounded">
+            <h5 class="text-white">Digital Illustration with Procreate</h5>
           </div>
         </div>
       <?php endif; ?>
@@ -48,18 +60,61 @@ $defaultImg = 'assets/images/skills/1.png';
   </div>
 
   <div class="row row-cols-1 row-cols-md-4 g-4">
-    <?php foreach ($skills as $s): $img = !empty($s['image_path']) ? $s['image_path'] : $defaultImg; ?>
+    <?php if ($skills): ?>
+      <?php foreach ($skills as $s): $img = !empty($s['image_path']) ? $s['image_path'] : $defaultImg; ?>
+        <div class="col">
+          <div class="card border-0 text-center">
+            <img src="<?php echo htmlspecialchars($img); ?>" class="card-img-top skill-img" alt="<?php echo htmlspecialchars($s['title']); ?>">
+            <div class="card-body">
+              <h6 class="card-title"><?php echo htmlspecialchars($s['title']); ?></h6>
+              <p class="card-text">Rate: $<?php echo number_format((float)$s['rate'], 2); ?>/hr</p>
+              <a href="details.php?id=<?php echo (int)$s['skill_id']; ?>" class="btn btn-sm btn-primary rounded-pill" style="background-color:#cd4f07; border:none;">View Details</a>
+            </div>
+          </div>
+        </div>
+      <?php endforeach; ?>
+    <?php else: ?>
       <div class="col">
         <div class="card border-0 text-center">
-          <img src="<?php echo htmlspecialchars($img); ?>" class="card-img-top skill-img" alt="<?php echo htmlspecialchars($s['title']); ?>">
+          <img src="assets/images/skills/1.png" class="card-img-top skill-img" alt="Beginner Guitar Lessons">
           <div class="card-body">
-            <h6 class="card-title"><?php echo htmlspecialchars($s['title']); ?></h6>
-            <p class="card-text">Rate: $<?php echo number_format((float)$s['rate'], 2); ?>/hr</p>
-            <a href="details.php?id=<?php echo (int)$s['skill_id']; ?>" class="btn btn-sm btn-primary rounded-pill" style="background-color:#cd4f07; border:none;">View Details</a>
+            <h6 class="card-title">Beginner Guitar Lessons</h6>
+            <p class="card-text">Rate: $30.00/hr</p>
+            <a href="details.php?id=1" class="btn btn-sm btn-primary rounded-pill" style="background-color:#cd4f07; border:none;">View Details</a>
           </div>
         </div>
       </div>
-    <?php endforeach; ?>
+      <div class="col">
+        <div class="card border-0 text-center">
+          <img src="assets/images/skills/6.png" class="card-img-top skill-img" alt="Digital Illustration with Procreate">
+          <div class="card-body">
+            <h6 class="card-title">Digital Illustration with Procreate</h6>
+            <p class="card-text">Rate: $40.00/hr</p>
+            <a href="details.php?id=6" class="btn btn-sm btn-primary rounded-pill" style="background-color:#cd4f07; border:none;">View Details</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-0 text-center">
+          <img src="assets/images/skills/7.png" class="card-img-top skill-img" alt="Morning Vinyasa Flow">
+          <div class="card-body">
+            <h6 class="card-title">Morning Vinyasa Flow</h6>
+            <p class="card-text">Rate: $35.00/hr</p>
+            <a href="details.php?id=7" class="btn btn-sm btn-primary rounded-pill" style="background-color:#cd4f07; border:none;">View Details</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-0 text-center">
+          <img src="assets/images/skills/8.png" class="card-img-top skill-img" alt="Intro to PHP & MySQL">
+          <div class="card-body">
+            <h6 class="card-title">Intro to PHP & MySQL</h6>
+            <p class="card-text">Rate: $55.00/hr</p>
+            <a href="details.php?id=8" class="btn btn-sm btn-primary rounded-pill" style="background-color:#cd4f07; border:none;">View Details</a>
+          </div>
+        </div>
+      </div>
+    <?php endif; ?>
   </div>
 </div>
 <?php include 'includes/footer.inc'; ?>
