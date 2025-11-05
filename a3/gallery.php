dc186b5b2e600defa7f0a4b7bc74fdf7e59ed71b
Huh <sentrooper@gmail.com>
2025-11-06 09:45:11 +1100

    Making website dynamic

--- a3/gallery.php
+++ a3/gallery.php
@@ -52,20 +52,9 @@
         </div>
       <?php endforeach; ?>
     </div>
-
-
-
-    
   <?php else: ?>
-    <div class="gallery">
-      <div class="gallery-item"><a href="#" data-image-modal="assets/images/skills/1.png"><img src="assets/images/skills/1.png" alt="Beginner Guitar Lessons"></a><p>Beginner Guitar Lessons</p></div>
-      <div class="gallery-item"><a href="#" data-image-modal="assets/images/skills/2.png"><img src="assets/images/skills/2.png" alt="Intermediate Fingerstyle"></a><p>Intermediate Fingerstyle</p></div>
-      <div class="gallery-item"><a href="#" data-image-modal="assets/images/skills/3.png"><img src="assets/images/skills/3.png" alt="Artisan Bread Baking"></a><p>Artisan Bread Baking</p></div>
-      <div class="gallery-item"><a href="#" data-image-modal="assets/images/skills/4.png"><img src="assets/images/skills/4.png" alt="French Pastry Making"></a><p>French Pastry Making</p></div>
-      <div class="gallery-item"><a href="#" data-image-modal="assets/images/skills/5.png"><img src="assets/images/skills/5.png" alt="Watercolor Basics"></a><p>Watercolor Basics</p></div>
-      <div class="gallery-item"><a href="#" data-image-modal="assets/images/skills/6.png"><img src="assets/images/skills/6.png" alt="Digital Illustration with Procreate"></a><p>Digital Illustration with Procreate</p></div>
-      <div class="gallery-item"><a href="#" data-image-modal="assets/images/skills/7.png"><img src="assets/images/skills/7.png" alt="Morning Vinyasa Flow"></a><p>Morning Vinyasa Flow</p></div>
-      <div class="gallery-item"><a href="#" data-image-modal="assets/images/skills/8.png"><img src="assets/images/skills/8.png" alt="Intro to PHP &amp; MySQL"></a><p>Intro to PHP &amp; MySQL</p></div>
+    <div class="alert alert-info">
+      <p>No skills available in the gallery yet. <a href="add.php">Add a skill</a> to get started!</p>
     </div>
   <?php endif; ?>
 </main>
--- a3/skills.php
+++ a3/skills.php
@@ -38,14 +38,9 @@
               </tr>
             <?php endforeach; ?>
           <?php else: ?>
-            <tr><td><a href="details.php?id=1">Beginner Guitar Lessons</a></td><td>Music</td><td>Beginner</td><td>30.00</td></tr>
-            <tr><td><a href="details.php?id=2">Intermediate Fingerstyle</a></td><td>Music</td><td>Intermediate</td><td>45.00</td></tr>
-            <tr><td><a href="details.php?id=3">Artisan Bread Baking</a></td><td>Cooking</td><td>Beginner</td><td>25.00</td></tr>
-            <tr><td><a href="details.php?id=4">French Pastry Making</a></td><td>Cooking</td><td>Expert</td><td>50.00</td></tr>
-            <tr><td><a href="details.php?id=5">Watercolor Basics</a></td><td>Art</td><td>Beginner</td><td>20.00</td></tr>
-            <tr><td><a href="details.php?id=6">Digital Illustration with Procreate</a></td><td>Art</td><td>Intermediate</td><td>40.00</td></tr>
-            <tr><td><a href="details.php?id=7">Morning Vinyasa Flow</a></td><td>Wellness</td><td>Intermediate</td><td>35.00</td></tr>
-            <tr><td><a href="details.php?id=8">Intro to PHP & MySQL</a></td><td>Programming</td><td>Expert</td><td>55.00</td></tr>
+            <tr>
+              <td colspan="4" class="text-center">No skills available yet. <a href="add.php">Add a skill</a> to get started!</td>
+            </tr>
           <?php endif; ?>
         </tbody>
       </table>
